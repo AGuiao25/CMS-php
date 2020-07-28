43de2a8344cba5cc71518d3dd571af142563a056
AGuiao25 <66593846+AGuiao25@users.noreply.github.com>
2020-07-29 07:34:58 +0800

    Added the initial requirements for the new functionality (comment section)

---
+++ admin/includes/view_all_comments.php
@@ -0,0 +1,105 @@
+<table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Author</th>
+                                        <th>Comment</th>
+                                        <th>Email</th>
+                                        <th>Status</th>
+                                        <th>In Response To</th>
+                                        <th>Date</th>
+                                        <th>Approve</th>
+                                        <th>Unapprove</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+<?php 
+$query = "SELECT * FROM comments";
+$query_comments = mysqli_query($connection, $query);
+while($row = mysqli_fetch_assoc($query_comments)){
+    $comment_id = $row['comment_id'];
+    $commentt_author = $row['comment_author'];
+    $comment_post_id = $row['comment_post_id'];
+    $comment_content = $row['comment_content'];
+    $comment_email = $row['comment_email'];
+    $comment_status = $row['comment_status'];
+    $comment_date= $row['comment_date'];
+ ?>
+ 
+
+
+
+
+                                <tr>
+                                    <td><?php echo $comment_id; ?></td>
+                                    <td><?php echo $commentt_author ; ?></td>
+                                    <td><?php echo  $comment_content; ?></td>
+                                    <td><?php echo $comment_email; ?></td>
+                                    <td><?php echo $comment_status; ?></td>
+                                    <?php 
+$query = "SELECT * FROM posts WHERE post_id = $comment_post_id" ;
+$query_post_id = mysqli_query($connection, $query);
+
+while($row= mysqli_fetch_assoc($query_post_id)) {
+    $post_id = $row['post_id'];
+    $post_title = $row['post_title'];
+    echo "<td><a href='../post.php?p_id= $post_id'>$post_title</a></td>";
+}
+
+
+?>
+                                    
+                                    <td><?php echo $comment_date; ?></td>
+                                    <?php echo "<td><a href='comments.php?approve=$comment_id'>Approve</a></td>"; ?>
+                                    <?php echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>"; ?>
+                                    <?php echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>"; ?>
+                          
+                            </tr>
+                            
+
+
+<?php }  ?>     
+                                </tbody>
+                            </table>
+
+
+                            <?php
+if(isset($_GET['approve'])) {
+    $the_comment_id = $_GET['approve'];
+
+    $query = "UPDATE comments SET comment_status = 'APPROVED' WHERE comment_id = $the_comment_id";
+    $delete_query = mysqli_query($connection, $query);
+     header("Location: comments.php");
+
+    if(!$delete_query) {
+        echo "Query Failed." . mysqli_error($connection);
+    }
+}
+
+if(isset($_GET['unapprove'])) {
+    $the_comment_id = $_GET['unapprove'];
+
+    $query = "UPDATE comments SET comment_status = 'UNAPPROVED' WHERE comment_id = $the_comment_id";
+    $delete_query = mysqli_query($connection, $query);
+     header("Location: comments.php");
+
+    if(!$delete_query) {
+        echo "Query Failed." . mysqli_error($connection);
+    }
+}
+
+if(isset($_GET['delete'])) {
+    $the_comment_id = $_GET['delete'];
+
+    $query = "DELETE FROM comments WHERE comment_id = {$the_comment_id}";
+    $delete_query = mysqli_query($connection, $query);
+     header("Location: comments.php");
+
+    if(!$delete_query) {
+        echo "Query Failed." . mysqli_error($connection);
+    }
+}
+
+?>
--- category.php
+++ category.php
@@ -15,8 +15,14 @@ include "includes/navigation.php";
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
+            <?php 
+            if(isset($_GET['category'])) {
+                $post_category_id = $_GET['category'];
+            }
+
+            ?>
             <?php
-        $query = "SELECT * FROM posts";
+        $query = "SELECT * FROM posts WHERE  post_category_id = $post_category_id ";
         $post_query = mysqli_query($connection,$query);
 
         while($row = mysqli_fetch_assoc($post_query)) {
@@ -25,12 +31,7 @@ include "includes/navigation.php";
             $post_author = $row['post_author'];
             $post_date = $row['post_date'];
             $post_image = $row['post_image'];
-            $post_content = substr($row['post_content'],0,100); 
-
-             
-             
-             ?>
-            
+            $post_content = substr($row['post_content'],0,100);  ?>
      
 
      
