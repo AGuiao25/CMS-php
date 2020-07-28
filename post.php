c1ce0ec80b515d7535f9c84b8de7e9fb90fc32e6
AGuiao25 <66593846+AGuiao25@users.noreply.github.com>
2020-07-29 07:32:00 +0800

    added comment form

--- post.php
+++ post.php
@@ -50,11 +50,20 @@ include "includes/navigation.php";
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
-                        <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                    <form action="" method="post" role="form">
+                    <div class="form-group">
+                    <label for="Author">Author</label>
+                            <input type="text" class="form-control" name="comment_author">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="form-group">
+                        <label for="Email">Email</label>
+                             <input type="text" class="form-control" name="comment_email">
+                        </div>
+                    <div class="form-group">
+                    <label for="Content">Content</label>
+                            <textarea class="form-control" rows="3" name="comment_content"></textarea>
+                        </div>
+                        <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
 
