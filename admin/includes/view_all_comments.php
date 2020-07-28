34ac5fdd70da8d53b2027130f023f3b70659064d
AGuiao25 <66593846+AGuiao25@users.noreply.github.com>
2020-07-29 07:51:32 +0800

    added new functionalities for comment section

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -6,11 +6,8 @@
                                         <th>Comment</th>
                                         <th>Email</th>
                                         <th>Status</th>
-<<<<<<< HEAD
                                         <th>In Response To</th>
-=======
                                         <th>In Response to</th>
->>>>>>> 6e76b74c8f2e0530549f2316b3dca3e16e03f9d1
                                         <th>Date</th>
                                         <th>Approve</th>
                                         <th>Unapprove</th>
@@ -21,7 +18,6 @@
 
 <?php 
 $query = "SELECT * FROM comments";
-<<<<<<< HEAD
 $query_comments = mysqli_query($connection, $query);
 while($row = mysqli_fetch_assoc($query_comments)){
     $comment_id = $row['comment_id'];
@@ -33,25 +29,13 @@ while($row = mysqli_fetch_assoc($query_comments)){
     $comment_date= $row['comment_date'];
  ?>
  
-=======
-$select_comments= mysqli_query($connection, $query);
-while($row = mysqli_fetch_assoc($select_comments)){
-    $comment_id= $row['comment_id'];
-    $comment_post_id = $row['comment_post_id'];
-    $comment_author = $row['comment_author'];
-    $comment_email= $row['comment_email'];
-    $comment_content = $row['comment_content'];
-    $comment_status = $row['comment_status'];
-    $comment_date = $row['comment_date'];
-    ?>
->>>>>>> 6e76b74c8f2e0530549f2316b3dca3e16e03f9d1
+
 
 
 
 
                                 <tr>
                                     <td><?php echo $comment_id; ?></td>
-<<<<<<< HEAD
                                     <td><?php echo $commentt_author ; ?></td>
                                     <td><?php echo  $comment_content; ?></td>
                                     <td><?php echo $comment_email; ?></td>
@@ -74,7 +58,6 @@ while($row= mysqli_fetch_assoc($query_post_id)) {
                                     <?php echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>"; ?>
                                     <?php echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>"; ?>
                           
-=======
                                     <td><?php echo $comment_author; ?></td>
                                     <td><?php echo $comment_email; ?></td>
                         
@@ -85,7 +68,6 @@ while($row= mysqli_fetch_assoc($query_post_id)) {
                                     <?php echo "<td><a href='comments.php?approve={$comment_id}'>Approve</a></td>"; ?>
                                     <?php echo "<td><a href='comments.php?inapprove={$comment_id}'>Unapprove</a></td>"; ?>
                                     <?php echo "<td><a href='comments.php?delete={  $comment_id}'>Delete</a></td>"; ?>
->>>>>>> 6e76b74c8f2e0530549f2316b3dca3e16e03f9d1
                             </tr>
                             
 
@@ -96,45 +78,12 @@ while($row= mysqli_fetch_assoc($query_post_id)) {
 
 
                             <?php
-<<<<<<< HEAD
-if(isset($_GET['approve'])) {
-    $the_comment_id = $_GET['approve'];
-
-    $query = "UPDATE comments SET comment_status = 'APPROVED' WHERE comment_id = $the_comment_id";
-    $delete_query = mysqli_query($connection, $query);
-     header("Location: comments.php");
-
-    if(!$delete_query) {
-        echo "Query Failed." . mysqli_error($connection);
-    }
-}
-
-if(isset($_GET['unapprove'])) {
-    $the_comment_id = $_GET['unapprove'];
-
-    $query = "UPDATE comments SET comment_status = 'UNAPPROVED' WHERE comment_id = $the_comment_id";
-    $delete_query = mysqli_query($connection, $query);
-     header("Location: comments.php");
-
-    if(!$delete_query) {
-        echo "Query Failed." . mysqli_error($connection);
-    }
-}
-
-if(isset($_GET['delete'])) {
-    $the_comment_id = $_GET['delete'];
-
-    $query = "DELETE FROM comments WHERE comment_id = {$the_comment_id}";
-    $delete_query = mysqli_query($connection, $query);
-     header("Location: comments.php");
-=======
 if(isset($_GET['delete'])) {
     $post_id = $_GET['delete'];
 
     $query = "DELETE FROM posts WHERE post_id = {$post_id}";
     $delete_query = mysqli_query($connection, $query);
     // header("Location: posts.php");
->>>>>>> 6e76b74c8f2e0530549f2316b3dca3e16e03f9d1
 
     if(!$delete_query) {
         echo "Query Failed." . mysqli_error($connection);
